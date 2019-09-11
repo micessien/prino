a4061787b479cf426ae81fdf4e0b51c65f8baf49
Micessien <micaeldie@gmail.com>
2019-09-11 14:33:49 +0000

    Clear cache

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,16 @@ use App\Project;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    return 'DONE'; //Return anything
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
 
 // Route::get('/projects/create', function () {
 
@@ -26,9 +29,6 @@ Route::get('/', function () {
 //     echo $user->projects->user_id;
 
 // });
-
-
-
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
