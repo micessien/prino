4ffa19f42895393dca88ce97983e8347466424b7
Micessien <micaeldie@gmail.com>
2019-09-13 23:49:48 +0000

    Clean well user blade

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -15,7 +15,7 @@
   <link rel="shortcut icon" href="assets/img/logo-prix-innovation.png" type="image/x-icon">
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <!-- <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <script src="https://cdn.tiny.cloud/1/przgmzfaioiisrc6rjnssfkp36q9iscbyi8az9573ti8fztu/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
   <script>
@@ -124,20 +124,10 @@
   </div>
 
   <script src="js/jquery.min.js"></script>
-  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/bootstrap.bundle.js"></script>
 
   <!-- Core plugin JavaScript -->
   <script src="assets/js/jquery.easing.min.js"></script>
-
-  <!--Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
-
-  <!-- Page level plugins -->
-  <script src="vendor/chart.js/Chart.min.js"></script>
-
-  <!-- Page level custom scripts -->
-  <script src="js/demo/chart-area-demo.js"></script>
-  <script src="js/demo/chart-pie-demo.js"></script>
 </body>
 
 </html>
