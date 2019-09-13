28a031c277da01c4bd9f23096c0a64fd3d51f426
Micessien <micaeldie@gmail.com>
2019-09-13 05:13:23 +0000

    Validation form register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'prenom' => 'required|string|max:255',
             'genre' => 'required',
+            'telephone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed|'
             ]);
