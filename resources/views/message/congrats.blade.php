ce1658c3e57e68eeea36a4e8c93fb7314143ee0d
Micessien <micaeldie@gmail.com>
2019-09-13 04:05:37 +0000

    Remove review text congrats

--- resources/views/message/congrats.blade.php
+++ resources/views/message/congrats.blade.php
@@ -9,7 +9,7 @@
                 <div class="col-md-8 col-md-offset-2">
                     <div class="site-heading text-center">
                         <p><i class="fa fa-check"></i></p>
-                        <h4>Félicitations! Vous êtes inscrit, vous pouvez désormais remplir le dossier</h4>
+                        <h4 style="text-transform: none">Félicitations! Vous êtes inscrit, vous pouvez désormais remplir le dossier</h4>
                         <a href="{{ route('login') }}" class="btn circle btn-primary btn-vert btn-lg padding-left-right">
                             Je remplis le dossier de candidature
                         </a>
