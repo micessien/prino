3b55eea67dc279e2742c2a9b44f0ef53c0dcf527
Micessien <micaeldie@gmail.com>
2019-09-13 23:57:15 +0000

    Clean home user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,132 +1,102 @@
 @extends('layouts.user')
 
 @section('content')
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
-            <div id="content">
-      
-              <!-- Topbar -->
-              <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-      
-                <!-- Sidebar Toggle (Topbar) -->
-                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                  <i class="fa fa-bars"></i>
-                </button>      
-      
-                <!-- Topbar Navbar -->
-                <ul class="navbar-nav ml-auto">
-      
-                  <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                  <li class="nav-item dropdown no-arrow d-sm-none">
-                    <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fas fa-search fa-fw"></i>
-                    </a>
-                    <!-- Dropdown - Messages -->
-      
-                  </li>
-      
-                
-                  <div class="topbar-divider d-none d-sm-block"></div>
-      
-                  <!-- Nav Item - User Information -->
-                  <li class="nav-item dropdown no-arrow">
-                    <a class="nav-link dropdown-toggle" href="/profile" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->prenom }} {{ Auth::user()->name }} </span>
-                      <!-- <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60"> -->
-                    </a>
-                    <!-- Dropdown - User Information -->
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                      <a class="dropdown-item" href="#">
-                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Profil
-                      </a>
-      
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                        Logout
-                      </a>
-                    </div>
-                  </li>
-      
-                </ul>
-      
-              </nav>
-              <!-- End of Topbar -->
-      
-              <!-- Begin Page Content -->
-              <div class="container-fluid">
-      
-                <!-- Page Heading -->
-                <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                  <h1 class="h3 mb-0 text-gray-800">Tableau de Bord</h1>
-                </div>
-      
-                <!-- Content Row -->
-                <div class="row">
-      
-                  <!-- Earnings (Monthly) Card Example -->
-                  <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card border-left-primary shadow h-100 py-2">
-                      <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                          <div class="col mr-2">
-                            <a href="projects/index">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Postuler</div>
-                          </a>
-                          </div>
-                          <div class="col-md-12 text-center">
-                              <img src="uploads/avatars/edit.png" width="100" height="100" alt="">
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+    <!-- Topbar -->
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-                  <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card border-left-info shadow h-100 py-2">
-                      <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                          <div class="col mr-2">
-                              <a href="profile">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Mon Profil</div>
-                              </a>
-                          </div>
-                          <div class="col-md-12 text-center">
-                              <img src="uploads/avatars/user.png" width="100" height="100" alt="">
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-      
-      
-      
-      
-                  </div>
-      
-      
-      
-              </div>
-              <!-- /.container-fluid -->
-      
-            </div>
-            </div>
-            <!-- End of Main Content -->
-      
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-              <div class="container my-auto">
-                <div class="copyright text-center my-auto">
-                  <span>Copyright &copy; prixinnovation 2019</span>
-                </div>
-              </div>
-            </footer>
-            <!-- End of Footer -->
-      
+      <!-- Sidebar Toggle (Topbar) -->
+      <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+        <i class="fa fa-bars"></i>
+      </button>
+
+      <!-- Topbar Navbar -->
+      <ul class="navbar-nav ml-auto">
+
+        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+        <li class="nav-item dropdown no-arrow d-sm-none">
+          <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-search fa-fw"></i>
+          </a>
+          <!-- Dropdown - Messages -->
+
+        </li>
+
+        <div class="topbar-divider d-none d-sm-block"></div>
+
+        <!-- Nav Item - User Information -->
+        <li class="nav-item dropdown no-arrow">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->prenom }} {{ Auth::user()->name }} </span>
+            <!-- <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60"> -->
+          </a>
+          <!-- Dropdown - User Information -->
+          <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+            <a class="dropdown-item" href="/profile">
+              <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+              Profil
+            </a>
+
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal" data-target="#logoutModal">
+              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              Logout
+            </a>
           </div>
-          <!-- End of Content Wrapper -->
+        </li>
+
+      </ul>
+
+    </nav>
+    <!-- End of Topbar -->
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+      <!-- Page Heading -->
+      <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Tableau de Bord</h1>
+      </div>
+
+      <!-- Content Row -->
+      <div class="row">
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <a href="projects/index">
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Postuler</div>
+                  </a>
+                </div>
+                <div class="col-md-12 text-center">
+                  <img src="uploads/avatars/edit.png" width="100" height="100" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <a href="profile">
+                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Mon Profil</div>
+                  </a>
+                </div>
+                <div class="col-md-12 text-center">
+                  <img src="uploads/avatars/user.png" width="100" height="100" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
 @endsection
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -93,7 +93,7 @@
   <footer class="sticky-footer bg-white">
     <div class="container my-auto">
       <div class="copyright text-center my-auto">
-        <span>Copyright &copy; Your Website 2019</span>
+        <span>Copyright &copy; prixinnovation 2019</span>
       </div>
     </div>
   </footer>
