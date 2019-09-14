3886ed775fea576d80dc2e5e55dda14cde997c38
Micessien <micaeldie@gmail.com>
2019-09-14 01:36:58 +0000

    Supprimer le champ Tyni into user

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -18,11 +18,6 @@
   <!-- <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <script src="https://cdn.tiny.cloud/1/przgmzfaioiisrc6rjnssfkp36q9iscbyi8az9573ti8fztu/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-  <script>
-    tinymce.init({
-      selector: 'textarea'
-    });
-  </script>
   <!-- Custom styles for this template -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
