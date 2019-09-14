c7802edf0c43df824953d34766f9a564d0d1430e
Micessien <micaeldie@gmail.com>
2019-09-14 02:35:42 +0000

    Visuel index 3

--- resources/views/projects/index3.blade.php
+++ resources/views/projects/index3.blade.php
@@ -14,20 +14,17 @@
             <div class="form-row">
                 <div class="form-group col-md-12">
                     <label for="equipe"> Combien de personnes comptez vous embaucher pour le développement de votre projet ?</label>
-                    <textarea name="equipe" value="{{ $user->projects->equipe }}" class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Descriptif des compétences nécessaires au développement du projet (700 signes maximum espaces compris)">
-                    </textarea>
-                </div>
-
-                <div class="form-group">
-                    <div class="form-check">
-                        <label class="form-check-label" for="gridCheck">
-                            Les données de votre entité fournies dans le cadre des prix de l’innovation du Ministère du Pétrole, de l’Energie et des Energies Renouvelables seront confidentielles. descriptif pourra être utilisé pour les différentes communications sur le concours. Seules les données à caractères publiques seront divulguées pour les besoins de publicité du concours.
-                            Tous les champs marqués « * » sont obligatoires.
-                        </label>
-                    </div>
+                    <textarea name="equipe" placeholder="Descriptif des compétences nécessaires au développement du projet (300 signes maximum espaces compris)" value="{{ $user->projects->equipe }}" class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Descriptif des compétences nécessaires au développement du projet (700 signes maximum espaces compris)"></textarea>
                 </div>
             </div>
-            
+            <div class="form-group">
+                    <p class="small">
+                            <i class="fa fa-exclamation-circle"></i>
+                            Les données fournies dans le cadre du prix de l’innovation du Ministère du Pétrole, de l’Energie et des Energies Renouvelables seront confidentielles. Le descriptif pourra être utilisé pour les différentes communications sur le concours. Seules les données à caractère public seront divulguées pour les besoins de publicité du concours.
+                            Tous les champs marqués « * » sont obligatoires.
+                    </p>
+            </div>
+
             <a href="{{ url('projects/index2') }}" class="btn btn-primary">Retour</a>
             <button type="submit" class="btn btn-primary">Suivant</button>
         </form>
